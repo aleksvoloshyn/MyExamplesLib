2805c7009a68054d7bee59f3aaa3ee942ae02f35
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2021-06-16 21:15:20 +0300

    всплывающее-окно

--- src/js/asynchrony/notification.js
+++ src/js/asynchrony/notification.js
@@ -1,20 +1,27 @@
-import '../css/common.css';
+// import './../../sass/layout/_notification.scss';
+import './../../sass/main.scss';
+// **
+//  * - Показываем и скрываем добавляя/удаляя класс is-visible
+//  * - Скрываем через определённое время
+//  * - Скрываем при клике
+//  * - Не забываем чистить таймер
+//  */
 
-/**
- * - Показываем и скрываем добавляя/удаляя класс is-visible
- * - Скрываем через определённое время
- * - Скрываем при клике
- * - Не забываем чистить таймер
- */
-
+// задержка
 const NOTIFICATION_DELAY = 3000;
+
+// обьявляем глобальную переменную
 let timeoutId = null;
+
+//объект ссылок
 const refs = {
   notification: document.querySelector('.js-alert'),
 };
 
+// функция по клику
 refs.notification.addEventListener('click', onNotificationClick);
 
+//вызов функции добавления класса
 showNotification();
 
 /*
